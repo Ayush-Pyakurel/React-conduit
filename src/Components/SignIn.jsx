c028300d07619e35eb80090586d4ed55cf01f544
Ayush Pyakurel <ayushpyakurel449@gmail.com>
2022-06-14 15:33:15 +0545

    Form validation for SignIn and SignUp is done

--- src/Components/SignIn.jsx
+++ src/Components/SignIn.jsx
@@ -1,11 +1,40 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import Link from 'react-router-dom/Link';
+import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+const schema = yup.object().shape({
+  email: yup.string().email('Email is invalid').required('Email is required'),
+  password: yup
+    .string()
+
+    .required('Password is required')
+    .matches(
+      /^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/,
+      'Password must contain at least 8 characters, one uppercase, one number and one special case character'
+    )
+    .min(6)
+    .max(12),
+});
 
 function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, touchedFields },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const formSubmit = (e) => {
+    console.log(e);
+  };
+
   return (
-    <Form className='signInForm'>
+    <Form className='signInForm' onSubmit={handleSubmit(formSubmit)}>
       <Form.Group className='mb-3' controlId='formBasicEmail'>
         <h2>Sign In</h2>
         <Link to='/signup' style={{ color: 'green', marginButton: '10px' }}>
@@ -15,8 +44,16 @@ function SignIn() {
           type='email'
           placeholder='Enter email'
           style={{ height: '50px' }}
+          name='email'
           required
+          {...register('email', { required: true }, touchedFields)}
         />
+        {errors.email ? (
+          <p style={{ color: 'red' }}>{errors.email?.message}</p>
+        ) : null}
+        {touchedFields.email && (
+          <p style={{ color: 'red' }}>Should enter the email</p>
+        )}
       </Form.Group>
 
       <Form.Group className='mb-3' controlId='formBasicPassword'>
@@ -24,8 +61,12 @@ function SignIn() {
           type='password'
           placeholder='Password'
           style={{ height: '50px' }}
+          name='password'
           required
+          {...register('password', { required: true }, touchedFields)}
         />
+        {errors.password ? <p>{errors.password?.message}</p> : null}
+        {touchedFields?.password && <p>Should enter Password</p>}
       </Form.Group>
       <Button variant='success' type='submit'>
         Submit
--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -2,21 +2,71 @@ import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+const schema = yup.object().shape({
+  username: yup.string().required('Username is required'),
+  email: yup.string().email('Email is invalid').required('Email is required'),
+  password: yup
+    .string()
+    .min(6)
+    .max(12)
+    .required('Password is required')
+    .matches(
+      /^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/,
+      'Password must contain at least 8 characters, one uppercase, one number and one special case character'
+    ),
+  confirmPassword: yup
+    .string()
+    .min(6)
+    .max(12)
+    .oneOf([yup.ref('password'), null], 'Passwords must match')
+    .required('Confirm Password is required'),
+});
 
 function SignUp() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const formSubmit = (e) => {
+    console.log(e);
+  };
+
   return (
-    <Form className='signUpForm'>
+    <Form className='signUpForm' onSubmit={handleSubmit(formSubmit)}>
+      <h2>Sign Up</h2>
+      <Link to='/signin' style={{ color: 'green', marginButton: '10px' }}>
+        Have an account?
+      </Link>
+      <Form.Group className='mb-3' controlId='formBasicUsername'>
+        <Form.Control
+          type='text'
+          placeholder='username'
+          style={{ height: '50px' }}
+          name='username'
+          required
+          {...register('username', { required: true })}
+        />
+        {errors.username?.message ? <p>{errors.username?.message}</p> : null}
+      </Form.Group>
+
       <Form.Group className='mb-3' controlId='formBasicEmail'>
-        <h2>Sign Up</h2>
-        <Link to='/signin' style={{ color: 'green', marginButton: '10px' }}>
-          Have an account?
-        </Link>
         <Form.Control
           type='email'
           placeholder='Enter email'
           style={{ height: '50px' }}
+          name='email'
           required
+          {...register('email', { required: true })}
         />
+        {errors.email ? <p>{errors.email?.message}</p> : null}
       </Form.Group>
 
       <Form.Group className='mb-3' controlId='formBasicPassword'>
@@ -24,8 +74,22 @@ function SignUp() {
           type='password'
           placeholder='Password'
           style={{ height: '50px' }}
+          name='password'
           required
+          {...register('password', { required: true })}
         />
+        {errors.password ? <p>{errors.password.message}</p> : null}
+      </Form.Group>
+      <Form.Group className='mb-3' controlId='formBasicMatchPassword'>
+        <Form.Control
+          type='password'
+          placeholder='Re-enter password'
+          style={{ height: '50px' }}
+          name='confirmPassword'
+          required
+          {...register('confirmPassword', { required: true })}
+        />
+        <p>{errors.confirmPassword?.message && 'Password should match!!'}</p>
       </Form.Group>
       <Button variant='success' type='submit'>
         Submit
